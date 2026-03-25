ccf36ef079f1fb7a418d78966cb2c2941d821cea
Steven Beeching <steven.beeching@gmail.com>
2026-03-25 13:35:56 +0000

    Improve deuggin

--- src/devices/rf-receive.py
+++ src/devices/rf-receive.py
@@ -1,13 +1,22 @@
 import time
 import sys
 import RPi.GPIO as GPIO
+from datetime import datetime
 
-RECEIVE_PIN = int(sys.argv[1])
+try:
+    RECEIVE_PIN = int(sys.argv[1])
+    print(f"Starting RF receiver on GPIO {RECEIVE_PIN}")
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(RECEIVE_PIN, GPIO.IN)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(RECEIVE_PIN, GPIO.IN)
 
-while True:
-    if GPIO.input(RECEIVE_PIN):
-        print('Received signal')
-    time.sleep(0.1)
+    while True:
+        if GPIO.input(RECEIVE_PIN):
+            print(f"[{datetime.now()}] Received signal: {GPIO.input(RECEIVE_PIN)}")
+        time.sleep(0.1)
+
+except Exception as e:
+    print(e)
+
+finally:
+    GPIO.cleanup()
